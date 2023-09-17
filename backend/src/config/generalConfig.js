2dbecccc26f39ef6a1d07aa6f9cd4d535aeb8ca4
sumitBansalk1 <sumit.bansal@indusnet.co.in>
2023-09-17 14:14:51 +0530

    added general config

---
+++ backend/src/config/generalConfig.js
@@ -0,0 +1,13 @@
+const config = {
+    userPerPage: 20,
+    libraryPerPage: 20,
+    folderPerPage: 20,
+    filePerPage: 20,
+    fileFolderList: 20,
+    saltRounds: 10,
+    defaultDueDate: "2090-07-29",
+    thumbnailWidth:350,
+    thumbnailHeight:200,
+}
+
+module.exports = config
